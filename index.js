ba6ed07a573b81c302ff20c48d8f9680c8384332
unknown <rajdeepb3300@gamil.com>
2022-09-12 23:33:52 +0530

    updated index

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const expressGraphQl = require("express-graphql").graphqlHTTP;
+const {GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLString, GraphQLList} = require("graphql");
+
+// importing user.json file to use the data
+const UserData = require("./user.json");
+
+const app = express();
+
+//Defining a User type
+
+const UserType = new GraphQLObjectType({
+    name: "User",
+    fields:() =>({
+        name: {type: GraphQLString},
+        location: {type: GraphQLString},
+        email: {type: GraphQLString},
+        phone: {type: GraphQLString},
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQuerytype",
+    fields: {
+        getAllUsers: {
+            type: new GraphQLList(UserType),
+            args: {name: {type: GraphQLString}},
+            resolve(parent, args){
+                return UserData
+            }
+        }
+    }
+});
+
+const schema = new GraphQLSchema({query: RootQuery});
+
+app.use("/", expressGraphQl({
+    schema,
+    graphiql: true
+}))
+
+app.listen(3000, function(){
+    console.log("Server is running");
+});
